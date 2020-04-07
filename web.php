3c8df0f35bbc5c11c9bb8a63f5a2def33db259ff
refaya704 <63276272+refaya704@users.noreply.github.com>
2020-04-07 11:19:23 +0700

    Menambahkan halaman depan/Beranda pada web

---
+++ web.php
@@ -0,0 +1,219 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="web.css">
+    <!-- javascript -->
+   <!--  <link rel="stylesheet" type="text/javascript" href="web.js"> -->
+    <!-- javascript -->
+
+
+    <!-- google fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Actor|Amaranth|Amatic+SC|Bangers|Gochi+Hand|Permanent+Marker|Press+Start+2P|Ultra&display=swap" rel="stylesheet"> 
+    <!-- google fonts -->
+
+
+     <script src="https://kit.fontawesome.com/23cefbcb34.js" crossorigin="anonymous"></script>
+    <title>---TK Singaraja---</title>  
+
+
+  </head>
+    <body>
+    <header>
+      <div class="main" >
+        <div class="logo"><img src="foto/logo1.png"></div>
+        <ul>
+          <li class="active"><a href="#BERANDA">BERANDA</a></li>
+          <li><a href="login/project3.html">PEMBAYARAN</a></li>
+          <li><a href="pendaftaran/awal.php">PENDAFTARAN</a></li>
+          <li><a href="login/project3.html">LOGIN</a></li>
+        </ul>
+      </div>
+
+      <div class="banner">
+        <video autoplay loop muted>
+          <source src="video1.mp4">
+        </video>
+      </div>
+
+      <div class="title">
+        <h1>TK TELKOM SINGARAJA</h1>
+      </div>
+    </header>
+    <!-- Pengumuman -->
+    <nav class="navbar-pengumuman">
+     <a class="navbar-brand" href="#">Pengumuman</a>
+    </nav>
+    <br><br>
+    <div class="container-pengumuman">
+      <div class="row">
+        <div class="col-md-3"><
+      <h2 class="judul-pengumuman">Selamat Datang</h2>
+      <br>
+      <img src="foto/pengumuman.jpeg" class="foto-pengumuman">
+    </div>
+
+    <div class="col-md-5 paragraf">
+      <p><i>Assalamualaikum Wr. Wb</i><br><br> Selamat datang di website kami TK Telkom Singaraja yang dapat digunakan sebagai salah satu media komunikasi, dan dapat dijadikan sebagai wahana interaksi yang positif antara civitas akademika maupun masyarakat pada umumnya <a href="#"><i>-Baca Selengkapnya</i></a></p>
+      <br>
+      <div class="nama">
+        Asep Subagja, S.Pd. - Kepala Sekolah
+      </div>
+    </div>
+
+    <div class="col-md-4">
+      <div class="container-pengumuman-2">
+       <h2 class="judul-pengumuman">Kegiatan</h2>
+        <br><br><br>
+        <i class="fa-5x far fa-calendar-alt ml-5 kalender"></i>
+        <br><br>
+        <h5 class="kalender">Tidak ada kegiatan</h5>
+         
+       </div>
+    </div>       
+    </div>
+    </div>
+    <br><br><br><br><br><br><br>
+    <!-- Galeri -->
+    <div class="galeri">
+      <br><br>
+      <h2 class="judul-galeri">Galeri Foto</h2>
+      <br><br>
+      <div class="row">
+        <div class="col-md-4">
+          <img src="foto/angkatan (1).png" class="foto1">
+          <div class="carousel-caption d-none d-md-block caption">
+           <h5>Wisuda Angkatan 3 <br> Tahun Pelajaran 2017-2018</h5>
+          </div>
+        </div>
+
+         <div class="col-md-4">
+          <img src="foto/gambar.png" class="foto1">
+          <div class="carousel-caption d-none d-md-block caption1">
+           <h5>Pelaksanaan kegiatan <br> Menggambar</h5>
+          </div>
+        </div>
+
+
+         <div class="col-md-4">
+          <img src="foto/olahraga.png" class="foto1">
+          <div class="carousel-caption d-none d-md-block caption2">
+           <h5>Pelaksanaan kegiatan <br> Olahraga Pagi</h5>
+          </div>
+        </div>
+      </div>
+      <br><br>
+      <div class="gambar">
+      <a href="#">
+    <h3>Lihat Semua Galeri</h3>
+  </a>
+</div>
+  <br><br>
+  </div>
+
+
+
+    <!-- Trending -->
+    <div class="container-news">
+    <div class="center">
+      <h1 class="judul">Trending</h1>
+      <div class="row">
+      <div class="col-md">
+      <div class="rekomendasi">
+          <h3 class="subjudul">Anak Umur 6 Tahun Ini Berhasil Menciptakan Robot</h3>
+          <img src="foto/trending_robot.jpg" class="foto">
+          <p class="deskripsi">
+          Seorang anak TK yang bernama Asep Saepudin  berhasil menciptakan sebuah robot pembantu. Dia bersekolah di TK Telkom Singaraja. Gurunya pun mengatakan.... <a href="#">Read more</a>
+          </p>
+        </div>
+      </div>
+       <div class="col-md">
+      <div class="rekomendasi-1">
+          <h3 class="subjudul">TK Telkom Singaraja Menguasai 5 Mendali Emas</h3>
+          <img src="foto/trending_renang.jpg" class="foto">
+          <p class="deskripsi">
+            Dalam perlombaan tingkat Provinsi, TK Telkom Singaraja lagi-lagi mendapatkan 5 emas. Inilah rahasianya
+          <a href="#">Read more</a>
+          </p>
+        </div>
+      </div>
+    </div>
+    </div>
+  </div>
+
+  <div class="tengah">
+    <h3 class="subjudul">Anak Ini Membuat Seniman Terkenal Diam</h3>
+          <img src="foto/trending-menggambar.jpg" class="foto">
+
+          <p class="deskripsi">
+            Dalam suatu acara, anak dari TK Telkom Singaraja menerima tantangan dari para seniman terkenal yaitu menggambar tangan dia sendiri dengan....
+           <a href="#">Read more</a>
+          </p>
+
+  </div>
+  <footer>
+      <div class="row">
+        <div class="col-md-6">      
+      <br>
+      <h3 class="judul-footer">TK Telkom Singaraja</h3>
+      <hr width="75%" class="garis">
+      <div class="map">
+      <div class="embed-responsive-16by9">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31598.997821736997!2d115.07366906885247!3d-8.114235776144401!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd19a0a9f2c383d%3A0x1278f6962e087f05!2sTK%20Sandhy%20Putra%20Singaraja!5e0!3m2!1sen!2sid!4v1586175035261!5m2!1sen!2sid" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe> 
+      </div>
+    </div>
+    </div>
+
+
+   <!--   <div class="col-md-3">      
+      <br>
+      <h3 class="judul-footer">Link Terkait</h3>
+      <hr width="75%" class="garis">
+      <br><br>
+      <p class="judul-footer">Kemdikbud <br> Facebook <br> Ypt.or.id <br>Dev.telkomschools <br> Silabus <br> Radarbali <br> Referensi.data</p>
+    </div> -->
+
+  <div class="col-md-4">      
+      <br>
+      <h3 class="judul-footer">Lokasi</h3>
+      <hr width="75%" class="garis">
+      <br>
+      <p class="judul-footer"><i class="fas fa-map-marker-alt mr-3"></i>JL. Gajah Mada No.122, Banjar Jawa, Kec. Buleleng, Kabupaten Buleleng, Bali 81113</p>
+      <br>
+       <p class="judul-footer"><i class="fas fa-phone mr-3"></i> (022)589670</p>
+       <br>
+       <p class="judul-footer"> <i class="fas fa-envelope mr-3"></i>TK_Singaraja@gmail.com</p>
+       <br>
+       <p class="judul-footer"> <i class="fab fa-instagram mr-3"></i></i>@ref_aya</p>
+       <br>
+       <p class="judul-footer"> <i class="fab fa-youtube mr-3"></i></i>TK Singaraja</p>
+    </div>
+  </div>
+  </footer>
+     
+
+   
+  
+
+
+
+
+
+
+
+  
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+   </body>
+</html>
